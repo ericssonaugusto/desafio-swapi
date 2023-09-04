ab4c3033bd7f892d94d653f0d029751c9a67c27e
Ericsson Augusto <ericssonaugusto@gmail.com>
2023-09-04 00:09:46 -0300

    Ajustato o apicache pra buscar toda a base

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,51 @@
-const cors = require('cors');
-const express = require('express');
+// Importa as bibliotecas necessárias
+const cors = require('cors'); // Biblioteca para lidar com CORS
+const express = require('express'); // Biblioteca para criar o servidor
 const app = express();
-const axios = require('axios');
-const apicache = require('apicache');
+const axios = require('axios'); // Biblioteca para fazer requisições HTTP
+const apicache = require('apicache'); // Biblioteca para fazer cache
 let cache = apicache.middleware;
 
-const port = 9009;
+const port = 9009; // Porta em que o servidor vai rodar
 
-app.use(cache('7 day'));
+// Configura o tempo de cache para 7 dias
+// Isso significa que os dados ficarão em cache por 7 dias antes de uma nova requisição ser feita à API
+app.use(cache('7 days'));
+
+// Ativa o CORS para todas as rotas
 app.use(cors());
 
-const swapi = 'https://swapi.dev/api/people/';
+// Função assíncrona para buscar todas as páginas da API Star Wars
+async function buscarTodasAsPaginas(urlInicial) {
+  let url = urlInicial;
+  const resultados = []; // Array para armazenar todos os resultados
 
+  // Enquanto houver uma URL para buscar, faça uma requisição
+  while (url) {
+    const { data } = await axios(url);
+    resultados.push(...data.results); // Adiciona os resultados no array
+    url = data.next; // Pega a próxima URL
+  }
+
+  return resultados; // Retorna todos os resultados quando terminar
+}
+
+// Rota para acessar os dados
 app.get('/ifaugusto/api', async (req, res) => {
-    try {
-        console.log('Tentando acessar a API Star Wars');
-        const { data } = await axios(swapi);
-        console.log('Dados recebidos com sucesso');
-        return res.json(data);
-    } catch (error) {
-        console.error('Erro ao acessar a API Star Wars:', error);
-        return res.status(500).json({ mensagem: 'Ocorreu um erro no servidor' });
-    }
+  try {
+    // Busca todos os dados usando a função buscarTodasAsPaginas
+    const todasAsPaginas = await buscarTodasAsPaginas('https://swapi.dev/api/people/');
+
+    // Envia os dados como resposta
+    res.json(todasAsPaginas);
+  } catch (error) {
+    // Em caso de erro, mostra o erro no console e envia uma resposta de erro
+    console.error('Erro ao acessar a API Star Wars:', error);
+    res.status(500).json({ mensagem: 'Ocorreu um erro no servidor' });
+  }
 });
 
+// Inicializa o servidor na porta definida
 app.listen(port, () => {
-    console.log(`Aplicação rodando na porta ${port}`);
+  console.log(`Aplicação rodando na porta ${port}`);
 });
