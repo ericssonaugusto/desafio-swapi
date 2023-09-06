1edf3ac1d8aa23521ecfebcfb94c12747bdc474f
Ericsson Augusto <ericssonaugusto@gmail.com>
2023-09-06 08:00:47 -0300

    refatorado front, melhorias no backend api backup

--- backend/server.js
+++ backend/server.js
@@ -1,51 +1,44 @@
-// Importa as bibliotecas necessárias
-const cors = require('cors'); // Biblioteca para lidar com CORS
-const express = require('express'); // Biblioteca para criar o servidor
-const app = express();
-const axios = require('axios'); // Biblioteca para fazer requisições HTTP
-const apicache = require('apicache'); // Biblioteca para fazer cache
+const cors = require('cors'); 
+const express = require('express'); 
+const axios = require('axios'); 
+const apicache = require('apicache'); 
+
+const app = express(); 
+const port = 9009; 
 let cache = apicache.middleware;
 
-const port = 9009; // Porta em que o servidor vai rodar
+app.use(cache('1 day')); 
+app.use(cors()); 
 
-// Configura o tempo de cache para 7 dias
-// Isso significa que os dados ficarão em cache por 7 dias antes de uma nova requisição ser feita à API
-app.use(cache('7 days'));
-
-// Ativa o CORS para todas as rotas
-app.use(cors());
-
-// Função assíncrona para buscar todas as páginas da API Star Wars
-async function buscarTodasAsPaginas(urlInicial) {
+async function buscarTodasAsPaginas(urlInicial, tempoLimite) { 
   let url = urlInicial;
-  const resultados = []; // Array para armazenar todos os resultados
+  const resultados = [];
 
-  // Enquanto houver uma URL para buscar, faça uma requisição
   while (url) {
-    const { data } = await axios(url);
-    resultados.push(...data.results); // Adiciona os resultados no array
-    url = data.next; // Pega a próxima URL
+    const { data } = await axios.get(url, { timeout: tempoLimite });
+    resultados.push(...data.results); 
+    url = data.next; 
   }
 
-  return resultados; // Retorna todos os resultados quando terminar
+  return resultados; 
 }
 
-// Rota para acessar os dados
-app.get('/ifaugusto/api', async (req, res) => {
-  try {
-    // Busca todos os dados usando a função buscarTodasAsPaginas
-    const todasAsPaginas = await buscarTodasAsPaginas('https://swapi.dev/api/people/');
-
-    // Envia os dados como resposta
-    res.json(todasAsPaginas);
+app.get('/ifaugusto/api', async (req, res) => { 
+  try { 
+    const todasAsPaginas = await buscarTodasAsPaginas('https://swapi.dev/api/people/', 10000); // 10 segundos
+    res.json(todasAsPaginas); 
   } catch (error) {
-    // Em caso de erro, mostra o erro no console e envia uma resposta de erro
-    console.error('Erro ao acessar a API Star Wars:', error);
-    res.status(500).json({ mensagem: 'Ocorreu um erro no servidor' });
+    console.error('Erro ao acessar a primeira API Star Wars:', error); 
+    try {
+      const todasAsPaginasBackup = await buscarTodasAsPaginas('https://swapi.py4e.com/api/people/', 10000); // 10 segundos como plano B
+      res.json(todasAsPaginasBackup); 
+    } catch (errorBackup) { 
+      console.error('Erro ao acessar a segunda API Star Wars:', errorBackup);
+      res.status(500).json({ mensagem: 'Ocorreu um erro no servidor' });
+    }
   }
-});
+}); 
 
-// Inicializa o servidor na porta definida
 app.listen(port, () => {
   console.log(`Aplicação rodando na porta ${port}`);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 // Importação de bibliotecas e componentes
 import './global.css';
-import logo from '/logo.svg'
-import React from 'react';
-import { Layout} from 'antd';
-import AppRoutes from './Routers'
+import logo from '/logo.png'
+import { Button, Modal, Layout} from 'antd';
+import { Home } from './Home';
+import User from './components/User';
+import React, {useState} from 'react';
+
 
 
 const { Content, Footer, Header } = Layout;
@@ -11,31 +13,38 @@ const { Content, Footer, Header } = Layout;
 // Componente App
 const App: React.FC = () => {
   /* const [searchTerm, setSearchTerm] = 'useState'<string>(''); */
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   return (
     <>
     <Layout className='height-full'>
       <Header className='header-container'>
       <div className='flex'>
-        <a href="#" target="_blank">
-          <img src={logo} className="logo" alt="Logo" />
-        </a>
-      <h1 className="star-font">StarWars</h1>
+      <a className='area-logo' href="/" target="_blank">
+      <img src={logo} className="logo" alt="Logo" />    
+      <div className='flex'><h1 className="titulo">API </h1> <span>swapi.dev</span> </div>   
+      
+      </a>
       </div>
-      {/* <HeaderBusca  setSearchTerm={setSearchTerm}/> */}
-      <nav>
-        <ul className='nav-menu'>
-          <a href="/"><li>Página inicial</li></a>
-          <a href="/user"><li>User</li></a>
-          
-        </ul>
-      </nav>
+      <Button type="primary" onClick={showModal}>
+        Login - Cadastro
+      </Button>
+      <Modal title="Usuário" footer={null} open={isModalOpen} onCancel={handleCancel}>
+        <User/>
+      </Modal>
+
       </Header>
       <Content>
-            <AppRoutes />     
+      <Home/>  
       </Content>
     <Footer className='footer-container'>
               Desafio FullStack ©2023 Created by Ericsson Augusto
-            </Footer>
+    </Footer>
     </Layout>
     
     
